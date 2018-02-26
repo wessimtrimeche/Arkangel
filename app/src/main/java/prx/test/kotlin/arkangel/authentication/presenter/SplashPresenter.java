46bfa2a6844964387b06086e19f64b34c11dcdb1
wessimtrimeche <wessim.trimeche@gmail.com>
2018-02-26 12:30:51 +0100

    mvp impl

---
+++ app/src/main/java/prx/test/kotlin/arkangel/authentication/presenter/SplashPresenter.java
@@ -0,0 +1,13 @@
+package prx.test.kotlin.arkangel.authentication.presenter;
+
+/**
+ * Created by wessim23 on 2/26/18.
+ */
+
+public interface MainPresenter {
+
+    void onResume();
+
+
+    void onDestroy();
+}
---
+++ app/src/main/java/prx/test/kotlin/arkangel/authentication/views/SplashView.java
@@ -0,0 +1,8 @@
+package prx.test.kotlin.arkangel.authentication.views;
+
+/**
+ * Created by wessim23 on 2/26/18.
+ */
+
+public interface MainView {
+}
