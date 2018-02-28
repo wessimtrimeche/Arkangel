595011c26c92f8b4eb44a65fd6331eec8a18d9d8
wessimtrimeche <wessim.trimeche@gmail.com>
2018-02-28 11:58:48 +0100

    firebase register

--- app/src/main/java/prx/test/kotlin/arkangel/authentication/view/RegisterActivity.kt
+++ app/src/main/java/prx/test/kotlin/arkangel/authentication/view/RegisterActivity.kt
@@ -1,26 +1,38 @@
 package prx.test.kotlin.arkangel.authentication.view
 
 import android.content.Intent
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v4.content.ContextCompat
+import android.support.v7.app.AppCompatActivity
+import android.util.Patterns
 import android.view.View
-import prx.test.kotlin.arkangel.R
-import kotlinx.android.synthetic.main.activity_authentication.*
 import android.view.ViewGroup.MarginLayoutParams
+import android.widget.Toast
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.android.gms.tasks.Task
+import com.google.firebase.auth.AuthResult
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseAuthUserCollisionException
+import kotlinx.android.synthetic.main.activity_authentication.*
+import prx.test.kotlin.arkangel.R
+import prx.test.kotlin.arkangel.profile.view.ProfileActivity
 
 
 class RegisterActivity : AppCompatActivity() {
 
+
+    val mAuth = FirebaseAuth.getInstance()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         getSupportActionBar()?.hide()
         setContentView(R.layout.activity_authentication)
 
+
         userTypeRadioGroup.setOnCheckedChangeListener { radioGroup, i ->
             when(i){
                 R.id.childRadioButton -> {
-                    email.visibility = View.GONE
-                    password.visibility = View.GONE
+                    emailEditText.visibility = View.GONE
+                    passwordEditText.visibility = View.GONE
                     goToLogin.visibility= View.GONE
                     parentCode.visibility=View.VISIBLE
 
@@ -32,8 +44,8 @@ class RegisterActivity : AppCompatActivity() {
 
                 }
                 R.id.parentsRadioButton->{
-                    email.visibility = View.VISIBLE
-                    password.visibility = View.VISIBLE
+                    emailEditText.visibility = View.VISIBLE
+                    passwordEditText.visibility = View.VISIBLE
                     goToLogin.visibility= View.VISIBLE
                     parentCode.visibility=View.GONE
                     registerBtn.setText("Register")
@@ -46,7 +58,64 @@ class RegisterActivity : AppCompatActivity() {
             val intent = Intent(this, LoginActivity::class.java)
             startActivity(intent)
         })
+
+        registerBtn.setOnClickListener({
+            registerUser()
+
+        })
     }
+
+
+    private fun registerUser() {
+        val email = emailEditText.text.toString().trim { it <= ' ' }
+        val password = passwordEditText.text.toString().trim { it <= ' ' }
+
+        if (email.isEmpty()) {
+            emailEditText.error = "Email is required"
+            emailEditText.requestFocus()
+            return
+        }
+
+        if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            emailEditText.error = "Please enter a valid email"
+            emailEditText.requestFocus()
+            return
+        }
+
+        if (password.isEmpty()) {
+            passwordEditText.error = "Password is required"
+            passwordEditText.requestFocus()
+            return
+        }
+
+        if (password.length < 6) {
+            passwordEditText.error = "Minimum lenght of password should be 6"
+            passwordEditText.requestFocus()
+            return
+        }
+
+
+        mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener { task: Task<AuthResult> ->
+            if (task.isSuccessful) {
+
+                finish()
+                startActivity(Intent(this@RegisterActivity, ProfileActivity::class.java))
+
+                val firebaseUser = mAuth.currentUser!!
+            } else {
+                if (task.getException() is FirebaseAuthUserCollisionException) {
+                    Toast.makeText(getApplicationContext(), "You are already registered", Toast.LENGTH_SHORT).show();
+
+                } else {
+                    Toast.makeText(getApplicationContext(), task.getException().toString(), Toast.LENGTH_SHORT).show();
+                }
+            }
+        }
+    }
+
+
+
+
 
 
 
