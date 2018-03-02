98be4fd274e5a5884b50694aeb7135bec69280aa
wessimtrimeche <wessim.trimeche@gmail.com>
2018-03-02 11:51:48 +0100

    profile photo storage with firebase & editProfile layout

--- app/src/main/java/prx/test/kotlin/arkangel/authentication/view/LoginActivity.kt
+++ app/src/main/java/prx/test/kotlin/arkangel/authentication/view/LoginActivity.kt
@@ -4,20 +4,15 @@ import android.app.ProgressDialog
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
 import android.view.WindowManager
 import android.widget.Toast
 import com.google.android.gms.tasks.Task
 import com.google.firebase.auth.AuthResult
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.FirebaseAuthUserCollisionException
-import kotlinx.android.synthetic.main.activity_authentication.*
 import kotlinx.android.synthetic.main.activity_login.*
 import prx.test.kotlin.arkangel.R
 import prx.test.kotlin.arkangel.authentication.presenter.AuthenticationPresenter
-import prx.test.kotlin.arkangel.profile.view.ProfileActivity
-
-
+import prx.test.kotlin.arkangel.profile.presenter.EditProfileActivity
 
 
 class LoginActivity : AppCompatActivity() , AuthenticationView {
@@ -65,7 +60,7 @@ class LoginActivity : AppCompatActivity() , AuthenticationView {
                     if (task.isSuccessful) {
 
                         finish()
-                        val intent = Intent(this@LoginActivity, ProfileActivity::class.java)
+                        val intent = Intent(this@LoginActivity, EditProfileActivity::class.java)
                         intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
                         startActivity(intent)
                         val firebaseUser = mAuth.currentUser!!
